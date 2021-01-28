854c885fade99234765681fe2a7ef2c4bb8d0cf6
samuel <blanksonsamuel@outlook.com>
2021-01-28 11:02:33 +0000

    first commit of project

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import '../../App.css';
+import Cards from '../Cards';
+import GeminiSection from '../GeminiSection';
+
+function Home() {
+    return (
+        <>
+            <GeminiSection />
+            <Cards />
+        </>
+    )
+}
+
+export default Home
